7665c1b96ecb7c16ac46ccdaea36e44eafe0c59a
Usamahg <usamahg@genstudents.org>
2024-10-31 16:10:42 +0000

    Final week 4

--- mini-projectV2.py
+++ mini-projectV2.py
@@ -23,7 +23,7 @@ with open("orders.csv", 'r') as file:
   for row in reader:
     Orders.append(row)
 
-#main menu
+#app
 print("""
 -----------------------------------------------------
           Please pick an option:
@@ -32,10 +32,10 @@ print("""
         3. orders menu
         0. Save and Exit
 -----------------------------------------------------""")
-#menu navigation
+
 Menu_selection = input("Enter")
-if(int(Menu_selection) == 0):
-#WEEK4 SAVE AND EXIT
+if(int(Menu_selection) == 0):   
+#0 Save and Exit
     with open("products.csv", mode='w') as csvfile:
         fieldnames = ["name", "price"]
         writer = csv.DictWriter(csvfile, fieldnames= fieldnames)
@@ -56,7 +56,7 @@ if(int(Menu_selection) == 0):
             writer.writerow(items)
     exit
 elif (int(Menu_selection) == 1): 
-    #products menu
+#1 product menu
     print("""
 -----------------------------------------------------
           Please pick an option:
@@ -66,19 +66,21 @@ elif (int(Menu_selection) == 1):
         4. Delete products
         0. Exit
 -----------------------------------------------------""")
-
     Menu_selection = input("Enter menu selection:")
-
     if (int(Menu_selection) == 0):
+    #1.0 Exit
             exit
-    elif (int(Menu_selection) == 1):                                           
+    elif (int(Menu_selection) == 1):  
+    #1.1 View all products                                         
         print(Product_list)
-    elif (int(Menu_selection) == 2):                                           
+    elif (int(Menu_selection) == 2):  
+    #1.2 Create a product                                         
         New_product = {}
         New_product["name"] = str(input("enter product name"))
         New_product["price"] = int(input("enter product price"))
         Product_list.append(New_product)  
-    elif (int(Menu_selection) == 3):                                         
+    elif (int(Menu_selection) == 3):              
+    #1.3 Update products                            
         for (i, item) in enumerate(Product_list, start=0):
             print(i, item)
         Product_selection = int(input("Enter product index to update?"))
@@ -91,14 +93,15 @@ elif (int(Menu_selection) == 1):
         if (aOrder != ''):
             del Product_list[Product_selection]['price']
             Product_list[Product_selection]['price'] = aOrder
-    elif (int(Menu_selection) == 4):                                             
+    elif (int(Menu_selection) == 4):   
+    #1.4 Delete products                                          
         for (i, item) in enumerate(Product_list, start=0):
             print(i, item)
         Product_list.pop(int(input("Enter product index to delete:")))
     else: 
         print("invalid option")
 elif (int(Menu_selection) == 2): 
-    #couriers menu
+#2 couriers menu
     print("""
 -----------------------------------------------------
           Please pick an option:
@@ -111,15 +114,19 @@ elif (int(Menu_selection) == 2):
     Menu_selection = input("Enter menu selection:")
 
     if (int(Menu_selection) == 0):
+    #2.0 Exit
             exit
-    elif (int(Menu_selection) == 1):                                            #1. create a product
+    elif (int(Menu_selection) == 1): 
+    #2.1 View all couriers                                         
         print(Courier_list)
-    elif (int(Menu_selection) == 2):     
+    elif (int(Menu_selection) == 2):  
+    #2.2 Create a courier   
         New_courier = {}
         New_courier["name"]= str(input('Enter courier name:'))
         New_courier["phone number"]= str(input('Enter courier\'s phone number:'))
         Courier_list.append(New_courier)             
-    elif (int(Menu_selection) == 3):                                            #3. update a product
+    elif (int(Menu_selection) == 3):                                          
+    #2.3 Update a courier   
         for (i, item) in enumerate(Courier_list, start=0):
             print(i, item)
         Courier_selection = int(input("Enter courier index to update?"))
@@ -132,7 +139,8 @@ elif (int(Menu_selection) == 2):
         if (New_Courier_number != ''):
             del Courier_list[int(Courier_selection)]['phone number']
             Courier_list[int(Courier_selection)]['phone number'] = New_Courier_number
-    elif (int(Menu_selection) == 4):                                             #4. delete a product
+    elif (int(Menu_selection) == 4):                                             
+    #2.4 Delete a courier   
         for (i, item) in enumerate(Courier_list, start=0):
             print(i, item)
         Courier_list.pop(int(input("Enter courier index to delete:")))
@@ -140,7 +148,7 @@ elif (int(Menu_selection) == 2):
         print("invalid option")
     
 elif (int(Menu_selection) == 3): 
-    #orders menu
+#3 orders menu
     print("""
 -----------------------------------------------------
           Please pick an option:
@@ -150,14 +158,18 @@ elif (int(Menu_selection) == 3):
         3. Update order status
         4. Update order info
         5. Delete order
+        0. Exit
 
 -----------------------------------------------------""")
     Menu_selection = input("Enter:")
     if (int(Menu_selection) == 0):
+    #3.0 exit
         exit
     elif (int(Menu_selection) == 1):                                            
+    #3.1 View all orders
         print(Orders)
     elif (int(Menu_selection) == 2):                                            
+    #3.2 New order
         NewOrder = {}
         NewOrder["customer name"] = str(input("Enter customer full name:"))
         NewOrder["customer address"] = str(input("Enter customer address:"))
@@ -171,22 +183,20 @@ elif (int(Menu_selection) == 3):
         for (i, item) in enumerate(Courier_list, start=0):
             print(i, item)
         Courier_index = int(input("Enter courier index to update?"))
-
         NewOrder["order status"] = str(input("Enter customer order status:"))
         Orders.append(NewOrder)
         print("Order saved!")                       
     elif (int(Menu_selection) == 3):
+    #3.3 Update orders status
         for item in enumerate(Orders):
             print(item)
-        Order_selection = int(input("Enter order index to update?"))
-        # print("current status of order " + str(Order_selection) + " : " + Orders[int(Order_selection)]["orderStatus"])
-        # del Orders[int(Order_selection)]['orderStatus']
-        # Orders[int(Order_selection)]['orderStatus'] = input("update status:") 
+        Order_selection = int(input("Enter order index to update?")) 
         for item in enumerate(Order_status_list):
             print(item)
         status_selection = int(input("Enter new status index?"))
         Orders[Order_selection]['order satus'] = Order_status_list[status_selection]
     elif (int(Menu_selection) == 4):
+    #3.4 Update orders info
         for item in enumerate(Orders):
             print(item)
         selection = int(input("Enter order index to update?"))
@@ -201,6 +211,7 @@ elif (int(Menu_selection) == 3):
                 aOrder[key] = New_product
         print(aOrder)
     elif (int(Menu_selection) == 5):
+    #3.5 delete order
         for item in enumerate(Orders):
             print(item)
         Orders.pop(int(input("Enter order index to delete?")))
